026e9063bf61865351d0ba52325ffee497c0f744
bunnycarrot699 <146919774+bunnycarrot699@users.noreply.github.com>
2023-12-12 00:40:54 +0200

    Create function Sketch based on an input number

--- sketch.js
+++ sketch.js
@@ -1,28 +1,35 @@
 const container=document.querySelector('.container');
-var slider=document.getElementById("myRange");
-slider.oninput=function(){
-container.textContent='';
-n=this.value;
-for(i=0;i<n;i++)
-{ 
-    const firstrow=document.createElement('div');
-    firstrow.classList.add('firstrow');
-    //firstrow.textContent=` ${i}`;
-    container.appendChild(firstrow);
-    for(j=0;j<n;j++)
-    {
-        const element=document.createElement('div');
-        element.classList.add('element');
-        element.textContent=``;
-        firstrow.appendChild(element);
-    }
-}
-const elements=document.querySelectorAll('.element');
-     for(i=0;i<elements.length; i++)
-elements[i].addEventListener('mouseover', (event)=>
+
+function Sketch(number)
 {
-    event.target.style.backgroundColor='purple';
-});
+    for(i=0;i<number;i++)
+    { 
+        const firstrow=document.createElement('div');
+        firstrow.classList.add('firstrow');
+        //firstrow.textContent=` ${i}`;
+        container.appendChild(firstrow);
+        for(j=0;j<number;j++)
+        {
+            const element=document.createElement('div');
+            element.classList.add('element');
+            element.textContent=``;
+            firstrow.appendChild(element);
+        }
+    }
+    const elements=document.querySelectorAll('.element');
+         for(i=0;i<elements.length; i++)
+    elements[i].addEventListener('mouseover', (event)=>
+    {
+        event.target.style.backgroundColor='purple';
+    });
+}
+Sketch(16);
+
+var slider=document.getElementById("myRange");
+
+slider.oninput=function(){
+   container.textContent='';
+   Sketch(this.value);
 } 
 
 
