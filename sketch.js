4a751effc962d78d5b0fbe7abc79d6a083fd748e
bunnycarrot699 <146919774+bunnycarrot699@users.noreply.github.com>
2023-12-15 18:41:04 +0200

    Customize radio buttons

--- sketch.js
+++ sketch.js
@@ -1,4 +1,18 @@
 const container=document.querySelector('.container');
+const bts=document.querySelectorAll('input[type=radio]');
+const colors=['#9F2B68', '#BF40BF', '#800020','	#702963','#CF9FFF', '#483248','#A95C68'];
+let functiedefacut = function how(event)
+{   
+    if(bts[0].checked){
+        changebColor(event.target, 1);
+    }
+     else if(bts[1].checked){
+        changebColor(event.target, +0.1);
+    }
+    else if(bts[2].checked){
+        rainbow(event.target);
+    }
+}
 
 function Sketch(number)
 {
@@ -12,16 +26,13 @@ function Sketch(number)
         {
             const element=document.createElement('div');
             element.classList.add('element');
-            element.textContent=``;
             firstrow.appendChild(element);
+            element.textContent=``;
         }
     }
     const elements=document.querySelectorAll('.element');
          for(i=0;i<elements.length; i++)
-    elements[i].addEventListener('mouseover', (event)=>
-    {
-        event.target.style.backgroundColor='purple';
-    });
+    elements[i].addEventListener('mouseover', functiedefacut)
 }
 Sketch(16);
 
@@ -30,7 +41,31 @@ var slider=document.getElementById("myRange");
 slider.oninput=function(){
    container.textContent='';
    Sketch(this.value);
+   const Restart=document.querySelector('#Restart')
 } 
+
+function changebColor(bColor, change){
+    const value = getComputedStyle(bColor).getPropertyValue("background-color");
+    const parts=value.match(/[\d.]+/g);
+    if (parts.length === 3){
+        parts.push(1);
+    }
+    parts[3]= parseFloat(parts[3])+ change;
+    bColor.style.backgroundColor=`rgba(${ parts.join(',') })`;
+}
+function makeallclear(bCol){
+    bColor.style.backgroundColor
+}
+
+function rainbow(bColor){
+   const a=Math.floor(Math.random()*colors.length)
+   bColor.style.backgroundColor=`${colors[a]}`;
+}
+
+
+
+
+
 
 
 
